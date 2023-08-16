4597d141583a9414af2f55894a25b04ee553f3f3
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-08-17 02:37:41 +0500

    connect with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express(); 
 
-app.get("/", (req, resp)=>{    
-    resp.send("App is Working")
-})
+const connectDB = async ()=>{
+    mongoose.connect('mongodb://0.0.0.0:27017/e-comm');
+    const productSchema = new mongoose.Schema({});
+    const Product = mongoose.model('product', productSchema);
+    const data = await Product.find()  
+    console.log(data);
+}
+connectDB()
 
 app.listen(5000)
