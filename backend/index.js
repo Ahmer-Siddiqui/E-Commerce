00dfeb2c6906bb65ad7266eadf2139149367784d
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-09-07 13:25:17 +0500

    Login API in backend

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-require('./db/config')
-const User = require('./db/User')
-const app = express(); 
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.post("/register", async(req, resp)=>{
-    let user = await User.create(req.body)
-    console.log(user);
+app.post("/register", async (req, resp) => {
+  let user = await User.create(req.body);
+  let result = user.toObject()
+  delete result.password
+  console.log(result);
 
-    resp.send(req.body)
-})
+  resp.send(req.body);
+});
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
+    }
+  }else{
+    resp.send({ result: "No User Found" });
+  }
+});
 
-app.listen(5000,()=>{
-    console.log(`Server is Running...`);
-})
- 
+app.listen(5000, () => {
+  console.log(`Server is Running...`);
+});
