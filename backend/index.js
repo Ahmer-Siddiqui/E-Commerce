7df1d36816f7b0c64431f494ec7199a93d6eceee
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-09-11 23:37:44 +0500

    Delete Product API

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,11 @@ app.get("/products", async (req, resp) => {
   }
 });
 
+app.delete("/product/:id",async(req, resp)=>{
+  let result = await Product.deleteOne({_id: req.params.id});
+  resp.send(result) 
+})
+
 app.listen(5000, () => {
   console.log(`Server is Running...`);
 });
