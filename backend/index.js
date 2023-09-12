ff88f83c39ac9c8a30988c882122715a3e472a5b
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-09-13 02:32:10 +0500

    Search API for Product

--- backend/index.js
+++ backend/index.js
@@ -58,6 +58,16 @@ app.put("/products/:id",async(req, resp)=>{
   let result = await Product.updateOne({_id: req.params.id},{$set:req.body});
   resp.send(result) 
 })
+app.get("/search/:key",async(req, resp)=>{
+  let result = await Product.find({
+    "$or":[
+      {name:{$regex: req.params.key}},
+      {company:{$regex: req.params.key}},
+      {category:{$regex: req.params.key}}
+    ]
+  })
+  resp.send(result)
+})
  
 app.listen(5000, () => {
   console.log(`Server is Running...`);
