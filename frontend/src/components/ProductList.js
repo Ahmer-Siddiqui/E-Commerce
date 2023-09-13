436c6375654658712ceb2a86a7291b0defeed94f
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-09-13 23:56:45 +0500

    integrate Search API for Product in frontend

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -10,20 +10,32 @@ const ProductList = () => {
     setProducts(result);
   };
 
-  const deleteProduct = async (id)=>{
-    let result = await fetch(`http://localhost:5000/product/${id}`,{
-          method: 'Delete',
-          // body: JSON.stringify({email,password}),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "Delete",
+      // body: JSON.stringify({email,password}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     result = await result.json();
-    if(result){
-      getProducts()
+    if (result) {
+      getProducts();
     }
     console.log(result);
-  }
+  };
+  const searchHandle = async (e) => {
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    }else{
+      getProducts();
+    }
+  };
 
   useEffect(() => {
     getProducts();
@@ -31,6 +43,12 @@ const ProductList = () => {
   return (
     <div className="product-list">
       <h3>Product List</h3>
+      <input
+        type="text"
+        className="search-product-box"
+        onChange={searchHandle}
+        placeholder="Search Product"
+      />
       <ul>
         <li>S.no</li>
         <li>Name</li>
@@ -38,7 +56,7 @@ const ProductList = () => {
         <li>Category</li>
         <li>Operation</li>
       </ul>
-      {products.map((item, index) => {
+      {products.length > 0 ? products.map((item, index) => {
         return (
           <ul key={index}>
             <li>{index + 1}.</li>
@@ -46,12 +64,19 @@ const ProductList = () => {
             <li>$ {item.price}</li>
             <li>{item.category}</li>
             <li>
-              <button onClick={()=>{deleteProduct(item._id)}}>Delete</button>
+              <button
+                onClick={() => {
+                  deleteProduct(item._id);
+                }}
+              >
+                Delete
+              </button>
               <Link to={`/update/${item._id}`}>Update</Link>
             </li>
           </ul>
         );
-      })}
+          
+      }) : <h1>No Result Found</h1>}
     </div>
   );
 };
